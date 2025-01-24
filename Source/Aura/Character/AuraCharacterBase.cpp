10e75b4834eb26014fca726d5f58b66558ccf373
TheWavesgod <xtr2811683@gmail.com>
2025-01-24 10:19:17 +0000

    First Commit

---
+++ Source/Aura/Character/AuraCharacterBase.cpp
@@ -0,0 +1,20 @@
+// Copyright Terren 
+
+
+#include "AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
