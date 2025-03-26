6f75fb27a392c19ea6b5abdd893a5b13f8979c5b
TheWavesgod <xtr2811683@gmail.com>
2025-03-26 01:10:01 +0000

    Gameplay Tasks

--- Source/Aura/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -13,7 +13,12 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
-	const bool bIsServer = HasAuthority(&ActivationInfo);
+	
+}
+
+void UAuraProjectileSpell::SpawnProjectile()
+{
+	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
 	if (!bIsServer) return;
 
 	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
@@ -35,8 +40,4 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 		
 		Projectile->FinishSpawning(SpawnTransform);
 	}
-	
-	
-
-	
 }
